6d5de3780986e39f436bf86dbbf0c70a044af222
Sherwin Dapito <dapito.sherwin@yahoo.com>
2017-03-18 12:44:13 +0800

    Revert To 2nd Commit

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ var mongoConnectionLocal  = 'mongodb://localhost:27017/token'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoConnectionLocal, (error, database) => { if(error) { console.log(error); }});
-asdasd
+
 var app = express();
-dfsdfsdf
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Set controllers
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
